935f2393f0524c7cb2b2f200912d8d8dc23fd5b7
ken505 <68226398+ken505@users.noreply.github.com>
2021-05-09 22:21:21 +0900

    wip:

---
+++ .history/components/Links_20210509154856.jsx
@@ -0,0 +1,88 @@
+import Image from "next/image";
+import classes from "./Links.module.css";
+import Link from "next/link";
+// import { Twittersvg } from "./Twittersvg";
+
+const ITEMS = [
+  {
+    href: "https://twitter.com/ken_tsx",
+    title: "Twitter",
+    src: "/twitter.svg",
+    width: "21",
+    height: "21",
+  },
+  {
+    href: "https://www.instagram.com/ken.tsx/",
+    title: "Instagram",
+    src: "/insta.svg",
+    width: "21",
+    height: "21",
+    description: "Account for cat photos",
+  },
+  {
+    href: "https://zenn.dev/ken505",
+    title: "Zenn",
+    src: "/twitter.svg",
+    width: "21",
+    height: "21",
+  },
+  {
+    href: "https://note.com/ken505",
+    title: "note",
+    src: "/twitter.svg",
+  },
+  {
+    href: "https://github.com/ken505",
+    title: "Github",
+    src: "/twitter.svg",
+  },
+  {
+    href: "https://my-links-phi.vercel.app/posts/aboutMe",
+    title: "About Ken",
+    src: "/twitter.svg",
+  },
+  { href: "/photos", title: "Photos", src: "/twitter.svg" },
+];
+
+// リンクタグの中にはチルドレンは一つ、っていうルールなのね。。
+export function Links() {
+  return (
+    <div className={classes.grid}>
+      {ITEMS.map((item) => {
+        return (
+          <Link href={item.href} key={item.href}>
+            <a>
+              <h2 className={classes.card}>
+                <Image
+                  className={classes.image}
+                  src={item.src}
+                  alt="twitter Logo"
+                  width={21}
+                  height={21}
+                />
+                {item.title}
+                {/* <p>{item.description}</p> */}
+              </h2>
+            </a>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
+// css も map で回ってるけどこれはいいのか？？
+
+// Image の補足
+// Image を使って public の svg を呼びに行ってるよ。
+// 色はとりあえず svg の fill に指定してる。
+// Image を使うとデプロイできないので一旦保留。なんで？
+
+{
+  /* <Image
+className={classes.image}
+src="/insta.svg"
+alt="instaglam Logo"
+width={22}
+height={22}
+/> */
+}
---
+++ .history/pages/photos_20210509164826.jsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import Image from "next/image";
+// "next/Image"とすると、dev では問題ないが、build で error になるよ。
+import classes from "../styles/Home.module.css";
+import { Tab } from "../components/Tab";
+import { Headline } from "../components/Headline";
+// import Head from "next/head";
+// import { Footer } from "../components/Footer";
+// import { Main } from "../components/Main";
+// import { Headline } from "../components/Headline";
+// import { Links } from "../components/Links";
+
+export default function Home() {
+  return (
+    <div className={classes.container}>
+      <Tab title="About Ken" />
+      <Headline title="Photos 😽" />
+      <Link href="/">
+        <h2 className={classes.card}>Back to links</h2>
+      </Link>
+      <Image
+        src="/images/photo2.jpg"
+        alt="Store Manager Rask's Photo"
+        width={300}
+        height={300}
+        // priority
+        // className="next-image"
+      />
+      <p>Store Manager Rask</p>
+      <Image
+        src="/images/photo.jpg"
+        alt="Assistant Store Manager Sakura's Photo"
+        width={500}
+        height={500}
+        priority
+        className="next-image"
+      />
+      <p>Store Manager Rask</p>
+    </div>
+  );
+}
