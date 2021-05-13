969115a0f4f2311b20b530a8d0ce8799f336c801
ken505 <68226398+ken505@users.noreply.github.com>
2021-05-13 15:28:50 +0900

    wip:

---
+++ .history/src/pages/index_20210513133223.jsx
@@ -0,0 +1,18 @@
+import classes from "../styles/Home.module.css";
+import { Tab } from "src/components/Tab/Tab";
+import { Main } from "src/components/Main/Main";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className={classes.container}>
+      <Tab title="Ken's links" />
+
+      <Main />
+    </div>
+  );
+}
+
+// {/* <Tab title="Ken's links" name="link" content="Link collection"/> */}
+// {/* metatag に props を持たせると、ブラウザ側で怒られる。 */}
+{/* meta tag の意味を再確認して、エラーの詳細を調べる */}
