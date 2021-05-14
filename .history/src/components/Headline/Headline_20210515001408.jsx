8496566c2d34c11ac51c89a401b9487ccf293b60
ken505 <68226398+ken505@users.noreply.github.com>
2021-05-15 01:24:00 +0900

    💄 Modify headline style.

---
+++ .history/src/components/Headline/Headline_20210515001408.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import classes from "./Headline.module.css";
+
+export function Headline(props) {
+  return (
+    <div>
+      <h1 className={classes.title}>{props.title}</h1>
+      <div >
+        <Image
+        className={classes.image}
+          src="/images/ken.jpg"
+          width={60}
+          height={60}
+        />
+      </div>
+    </div>
+  );
+}
