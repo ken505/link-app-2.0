8a2fe2d141f4af32c358817692263691f59dd08a
ken505 <68226398+ken505@users.noreply.github.com>
2021-05-18 16:26:15 +0900

    💄 Add pc layout.

---
+++ .history/src/components/Headline/Headline_20210518160719.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import classes from "./Headline.module.css";
+
+export function Headline(props) {
+  return (
+    <div>
+      <h1 className={classes.title}>{props.title}</h1>
+      <div className={classes.image}>
+        <Image
+          src="/images/ken.jpg"
+          width={60}
+          height={60}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ .history/src/components/Headline/Headline_20210518160952.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import classes from "./Headline.module.css";
+
+export function Headline(props) {
+  return (
+    <div>
+      <h1 className={classes.title}>{props.title}</h1>
+      <div className={classes.image}>
+        {/* <Image
+          className={classes.icon}
+          src="/images/ken.jpg"
+          width={60}
+          height={60}
+        /> */}
+        <Image
+          className={classes.icon}
+          src="/images/ken.jpg"
+          width={60}
+          height={60}
+        />
+      </div>
+    </div>
+  );
+}
